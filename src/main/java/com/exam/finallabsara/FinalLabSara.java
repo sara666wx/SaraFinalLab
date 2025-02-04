2fe8c0519bbafe2950a91172899b0d25c8e22f72
sara666wx <sara666wx@gmail.com>
2025-02-04 20:55:21 +0300

    first commit

---
+++ src/main/java/com/exam/finallabsara/FinalLabSara.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.exam.finallabsara;
+
+/**
+ *
+ * @author SMART
+ */
+public class FinalLabSara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
